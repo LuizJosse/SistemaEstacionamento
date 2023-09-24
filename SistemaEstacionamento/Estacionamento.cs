43187d763620a8036eaa55039afa52a85fafe61e
luizn <luizn@DELLLUIZ>
2023-09-24 15:03:09 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaEstacionamento/Estacionamento.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Estacionamento
+{
+    public decimal precoInicial;
+    public decimal precoPorHora;
+   public List<string> veiculos;
+
+    public Estacionamento(decimal precoInicial, decimal precoPorHora)
+    {
+        this.precoInicial = precoInicial;
+        this.precoPorHora = precoPorHora;
+        veiculos = new List<string>();
+    }
+
+    public void AdicionarVeiculo(string placa)
+    {
+        veiculos.Add(placa);
+        Console.WriteLine($"Veículo com a placa {placa} foi estacionado.");
+    }
+
+    public void RemoverVeiculo(string placa)
+    {
+        if (veiculos.Contains(placa))
+        {
+            Console.Write("Digite a quantidade de horas que o veículo permaneceu estacionado: ");
+            if (decimal.TryParse(Console.ReadLine(), out decimal horas))
+            {
+                decimal valorCobrado = precoInicial + (precoPorHora * horas);
+                Console.WriteLine($"Valor cobrado para o veículo com a placa {placa}: R$ {valorCobrado:F2}");
+                veiculos.Remove(placa);
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("O veículo não está estacionado.");
+        }
+    }
+
+    public void ListarVeiculos()
+    {
+        if (veiculos.Count > 0)
+        {
+            Console.WriteLine("Veículos estacionados:");
+            foreach (string placa in veiculos)
+            {
+                Console.WriteLine($"Placa: {placa}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Não há veículos estacionados.");
+        }
+    }
+}
---
+++ SistemaEstacionamento/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o preço inicial: ");
+        if (decimal.TryParse(Console.ReadLine(), out decimal precoInicial))
+        {
+            Console.Write("Digite o preço por hora: ");
+            if (decimal.TryParse(Console.ReadLine(), out decimal precoPorHora))
+            {
+                Estacionamento estacionamento = new Estacionamento(precoInicial, precoPorHora);
+
+                while (true)
+                {
+                    Console.WriteLine("\nOpções:");
+                    Console.WriteLine("1 - Cadastrar veículo");
+                    Console.WriteLine("2 - Remover veículo");
+                    Console.WriteLine("3 - Listar veículos");
+                    Console.WriteLine("4 - Encerrar");
+
+                    Console.Write("Escolha uma opção: ");
+                    string opcao = Console.ReadLine();
+
+                    switch (opcao)
+                    {
+                        case "1":
+                            Console.Write("Digite a placa do veículo: ");
+                            string placa = Console.ReadLine();
+                            estacionamento.AdicionarVeiculo(placa);
+                            break;
+                        case "2":
+                            Console.Write("Digite a placa do veículo a ser removido: ");
+                            placa = Console.ReadLine();
+                            estacionamento.RemoverVeiculo(placa);
+                            break;
+                        case "3":
+                            estacionamento.ListarVeiculos();
+                            break;
+                        case "4":
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Opção inválida. Tente novamente.");
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Preço por hora inválido.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Preço inicial inválido.");
+        }
+    }
+}
